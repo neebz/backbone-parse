89e40676b868eaf542b58f76239008cd305749e3
neebz <muneeb83@gmail.com>
2015-04-20 00:46:51 +0500

    added fail reported to gulp-lint

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp   = require('gulp');
 gulp.task('lint', function() {
   return gulp.src('./backbone-parse.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('default', ['lint']);
